d195b507c4a97c9b72d896b52791f2205d930c0f
Liam Sho <conan_creeper@outlook.com>
2023-10-08 10:24:44 +0800

    feat: try to change the way to build image

--- build-docker-daemon.sh
+++
@@ -1,5 +0,0 @@
-VERSION="$1"
-VERSION="${VERSION#[vV]}"
-
-docker buildx build --load --platform linux/amd64 -t mcsmanager-daemon:${VERSION}-amd64 -f Dockerfile.daemon .
-docker buildx build --load --platform linux/arm64 -t mcsmanager-daemon:${VERSION}-arm64 -f Dockerfile.daemon .
--- build-docker-web.sh
+++
@@ -1,5 +0,0 @@
-VERSION="$1"
-VERSION="${VERSION#[vV]}"
-
-docker buildx build --load --platform linux/amd64 -t mcsmanager-web:${VERSION}-amd64 -f Dockerfile.web .
-docker buildx build --load --platform linux/arm64 -t mcsmanager-web:${VERSION}-arm64 -f Dockerfile.web .
